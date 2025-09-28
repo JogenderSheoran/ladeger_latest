afe955a40b79f01ef9bbacd260ac5303f6cea42b
Jogender <Jogenderdelaine@gmail.com>
2025-09-28 10:13:34 +0530

    heaader and name changes

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
 
     <head>
         <meta charset="utf-8" />
-        <title>{{ config('app.name') }} - Dashboard</title>
+        <title>RcLedger - Dashboard</title>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <meta content="Preview page of Metronic Admin Theme #3 for statistics, charts, recent events and reports" name="description" />
